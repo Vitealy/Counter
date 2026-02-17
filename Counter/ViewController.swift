f28bbd39b6c63b1cd62e2d2618884c6dc5fdcd92
Vitaly Kashavkin <kashavkin_v.v@mail.ru>
2026-02-17 20:13:21 +0500

    Добавлена базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    var scoreCounter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.font = UIFont.systemFont(ofSize: 100)
+        counterLabel.text = "\(scoreCounter)"
+        
     }
 
-
+    @IBAction func counterButton(_ sender: Any) {
+        scoreCounter += 1
+        counterLabel.text = "\(scoreCounter)"
+    }
+    
 }
 
