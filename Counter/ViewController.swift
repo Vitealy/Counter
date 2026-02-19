e534059b4342b222f5cdbbec46334802ce13e005
Vitaly Kashavkin <kashavkin_v.v@mail.ru>
2026-02-19 11:58:34 +0500

    Исправлена орфография и удалены лишние текстовые описания(подсказки)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,16 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
-    var scoreCounter: Int = 0  // Счетчик для отслеживания количества нажатий
+    private var scoreCounter: Int = 0  // Счетчик для отслеживания количества нажатий
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Настройка шрифта и начального значения для метки
         counterLabel.font = UIFont.systemFont(ofSize: 100)
         counterLabel.text = "\(scoreCounter)"
         
     }
-
+    
     @IBAction func counterButton(_ sender: Any) {
         // Увеличиваем счетчик на 1 при каждом нажатии на кнопку
         scoreCounter += 1
